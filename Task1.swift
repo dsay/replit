75a92d2d956d0a86d8d8682917a03951d137cfa8
Dima Sai <>
2022-05-12 13:00:13 +0000

    Initial commit

---
+++ Task1.swift
@@ -0,0 +1,8 @@
+let array: [[Int?]] = [[1, 2, nil], [3, 4], [5, 6, nil], [7, 8, 9]]
+
+func solution(_ array: [[Int?]]) -> Int {
+    return array 
+}
+
+print(solution(array) == 45)
+
---
+++ Task2.swift
@@ -0,0 +1,7 @@
+let array: [Any] = [1, 2, 3, 4, [5, 6], [7, [8, 9]]]
+
+func solution(_ array: [Any]) -> [Int] {
+    return array
+}
+
+print(solution(array) == [1, 2, 3, 4, 5, 6, 7, 8, 9])
